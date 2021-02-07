d51d1eae80da2b1cfaa1cd97fae3296603df08ba
gsk3 <gsk3@WKC001845967.ap.cat.com>
2021-02-07 12:22:46 +0530

    new

--- GitProject/src/com/myfirstpackage/Firstproject.java
+++ GitProject/src/com/myfirstpackage/Firstproject.java
@@ -5,3 +5,4 @@ public class Firstproject {
 		System.out.println("Hello World.");
 
 }
+}
---
+++ GitProject/src/com/myfirstpackage/learning.java
@@ -0,0 +1,27 @@
+package com.myfirstpackage;
+
+public class learning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myFirstNum= 45;
+		int mySecNum = 45;
+		
+		if (myFirstNum < mySecNum) {
+			System.out.println(myFirstNum + " < " + mySecNum);
+	} else if (myFirstNum > mySecNum) {
+		System.out.println(myFirstNum + " > " + mySecNum);
+	}else if (myFirstNum == mySecNum) {
+		System.out.println(myFirstNum + " == " + mySecNum);
+	}else if (myFirstNum <= mySecNum) {
+		System.out.println(myFirstNum + " <= " + mySecNum);
+	}else if (myFirstNum <= mySecNum) {
+		System.out.println(myFirstNum + " >= " + mySecNum);
+	}
+			
+			
+			
+
+	}
+
+}
