ec74678c26d32304ad050b3bb69c27971fdfb0cf
gsk3 <gsk3@WKC001845967.ap.cat.com>
2021-02-09 18:54:05 +0530

    update

--- GitProject/src/com/myfirstpackage/Tuesdayproblem.java
+++ GitProject/src/com/myfirstpackage/Tuesdayproblem.java
@@ -3,22 +3,30 @@ package com.myfirstpackage;
 public class Tuesdayproblem {
 
 	public static void main(String[] args) {
-		int myNumb1 = 2;
+		int myNumb1 = 4;
 		int myNumb2 = 3;
-		int myNumb3 = 4;
+		int myNumb3 = 23;
 		
 		
-		if ((myNumb1 > myNumb3))
-		{    System.out.println("2 is the greatest number");
+		if ((myNumb1 > myNumb2) && (myNumb1 > myNumb3))
+		{
+			System.out.println("myNumb1 is the greatest number");
 		}
-		else if ((myNumb2 > myNumb3))
-		{    System.out.println("2 is the greatest number");
+		
+		else if ((myNumb2 > myNumb1) && (myNumb2 > myNumb3))
+		
+		{
+			System.out.println("myNumb2 is the greatest number");
 		}
-		else
-		{    System.out.println("4 is the greatest number");
+		else 
+		{
+			System.out.println("myNumb3 is the greatest number");
 		}
-		}
-
 	}
+}
+	
+
+
+	
 
 
