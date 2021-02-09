f875d97b2ace559dc4427add677eb8f028d00e0c
gsk3 <gsk3@WKC001845967.ap.cat.com>
2021-02-09 17:35:51 +0530

    Tuesday task

--- GitProject/src/com/myfirstpackage/Mondayexc1.java
+++ GitProject/src/com/myfirstpackage/Mondayexc1.java
@@ -2,23 +2,23 @@ package com.myfirstpackage;
 
 public class Mondayexc1 {
 
-	private static final char f = 0;
-	private static final char p = 0;
+	
+
 
 	public static void main(String[] args) {
-		char ='p';
 		
-		string Myresult;		 
+		char Myresult;		 
+		
 		Myresult ='p';
 	 
 		
-		if (Myresult==p) {
+		if (Myresult=='p') {
 			
 			System.out.println(" if Myresult is p = Pass");
 			
 		}
 		
-		else if (Myresult==f)
+		else if (Myresult=='f')
 		{
 			System.out.println(" if Myresult is f = fail");
 		}
@@ -30,4 +30,4 @@ public class Mondayexc1 {
 
 	}
 
-}
+
---
+++ GitProject/src/com/myfirstpackage/Tuesdayproblem.java
@@ -0,0 +1,24 @@
+package com.myfirstpackage;
+
+public class Tuesdayproblem {
+
+	public static void main(String[] args) {
+		int myNumb1 = 2;
+		int myNumb2 = 3;
+		int myNumb3 = 4;
+		
+		
+		if ((myNumb1 > myNumb3))
+		{    System.out.println("2 is the greatest number");
+		}
+		else if ((myNumb2 > myNumb3))
+		{    System.out.println("2 is the greatest number");
+		}
+		else
+		{    System.out.println("4 is the greatest number");
+		}
+		}
+
+	}
+
+
